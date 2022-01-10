7990f0b7702edb5e93d378fe132ea5a89199b5e3
Charlie <cgigante@users.noreply.github.com>
2022-01-10 11:27:34 -0800

    Site updates - Jan 2022

---
+++ _vote.php
@@ -0,0 +1,13 @@
+<!-- Main Content -->
+    <div class="jumbotron">
+        <center>
+            <h1 style="font-size:5em;">Vote</h1>
+            <h4 class="jumbotron-text"><i class="fas fa-vote-yea"></i> Please register to vote!</h4>
+        </center>
+    </div>
+    <div class="clear"></div>
+    <div class="row">
+        <div class="col-sm-12">
+            <iframe src="https://register.vote.org/?partner=111111&campaign=free-tools" width="100%" marginheight="0" frameborder="0" id="frame1" scrollable ="no"></iframe><script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js"></script><script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>
+        </div>
+    </div>
